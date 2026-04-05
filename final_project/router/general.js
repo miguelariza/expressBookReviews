d70b9ebb5b21887ebcc358ba67582b998ad8d3e1
miguelariza <miguelangel.ariza@gmail.com>
2026-04-05 19:08:50 -0400

    Update register user and validate user

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,21 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        let userWithSameName = users.filter((user) => {
+            return user.username === username;     
+        });
+        if(userWithSameName.length > 0) {
+             return res.status(404).json({message: "The username already exist, choose another."});
+        } else {
+             users.push({"username": username, "password": password});
+             return res.status(200).json({message: "User successfully registered. Now you can login."});
+        }
+    }
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
