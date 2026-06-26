99f65192436caebdb3c3d5185d7eea06ee0d6488
miguelariza <miguelangel.ariza@gmail.com>
2026-06-26 00:00:37 -0400

    Fix in general.js file, repeated books const declaration

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,4 @@
 const express = require('express');
-let books = require("./booksdb.js");
 const books = require('./booksdb.js');
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
